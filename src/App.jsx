82dad67f321ac590139cc9c0cc5e9eb4d036851a
Renanppcosta <renanppcosta@gmail.com>
2024-08-14 18:47:44 -0300

    feat: initial project settings

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './assets/sass/index.scss'
+function App() {
+  return (
+    <>
+      <div className="w-full h-32">
+        <h1 className="text-3xl text-white"> &lt;renanppcosta/&gt; </h1>
+      </div>
+    </>
+  )
+}
+
+export default App

e096e54fbffd99e4891f647de07ce6d0fdf7d3eb
Renanppcosta <renanppcosta@gmail.com>
2024-08-19 19:52:59 -0300

    feat: hamburguer menu created

---
+++ src/components/HamburgerMenu/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export default function HamburgerMenu() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <button className="md:hidden" onClick={toggleMenu}>
+                <i className="fa-solid fa-bars text-white text-3xl"></i>
+            </button>
+
+            <nav
+                className={`nav-mobile w-full gap-5 flex items-center justify-center flex-col fixed top-0 left-0 z-50 duration-500 ${
+                    isOpen ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+                }`}
+            >
+                <button className="mt-4" onClick={toggleMenu}>
+                    <i className="fa-solid fa-x text-white text-xl"></i>
+                </button>
+
+                <ul className="flex items-center flex-col h-full gap-5">
+                    <li className="text-white"><a href="#">Sobre</a></li>
+                    <li className="text-white"><a href="#">Projetos</a></li>
+                    <li className="text-white"><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </>
+    );
+}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,19 @@
+import HamburgerMenu from "../HamburgerMenu"
+
 export default function Header(){
     return(
         <>
-            <header className="w-full h-24 flex justify-center items-center">
+            <header className="w-full h-24 flex justify-center items-center px-5">
                 <div className="w-full max-w-7xl flex justify-between">
-                    <h1 className="text-3xl text-white hover:rotate-3 duration-300 cursor-pointer"> &lt;renanppcosta/&gt; </h1>
+                    <h1 className="text-2xl md:text-3xl text-white hover:rotate-3 duration-300 cursor-pointer"> &lt;renanppcosta/&gt; </h1>
                     <nav>
-                        <ul className="flex items-center h-full gap-16">
+                        <ul className="hidden md:flex items-center h-full gap-16">
                             <li className="text-white"><a href="#">Sobre</a></li>
                             <li className="text-white"><a href="#">Projetos</a></li>
                             <li className="text-white"><a href="#">Contato</a></li>
                         </ul>
                     </nav>
+                    <HamburgerMenu />
                 </div>
             </header>
         </>
